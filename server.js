27c8668ebe1e8dc77db61a416d42039b74093534
Ergin <mesarikaya@gmail.com>
2017-01-13 23:18:09 +0000

    Second commit

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var express = require('express')
 var app = express()
+var moment = require('moment')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+//app.use(express.static(__dirname + "/../time_stamp_service"));
+
+app.get('/:timestamps', function (req, res) {
+    var tijd = moment(req.param.timestamps).format("LL")
+    var msg = "Unix: " + moment(req.param.timestamps) + ", Natural: " + tijd;
+    res.send(msg)
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 8080!')
 })
